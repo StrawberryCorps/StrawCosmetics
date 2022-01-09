75ddbd28e150c30fc71d44e2220c62feb901559c
Eclixal <28450187+Eclixal@users.noreply.github.com>
2022-01-09 20:17:46 +0100

    add log4j le poto

--- src/main/java/bzh/strawberrycorps/cosmetics/StrawCosmetics.java
+++ src/main/java/bzh/strawberrycorps/cosmetics/StrawCosmetics.java
@@ -1,4 +1,22 @@
 package bzh.strawberrycorps.cosmetics;
 
-public class StrawCosmetics {
+import lombok.Getter;
+import lombok.extern.log4j.Log4j;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Log4j
+public class StrawCosmetics extends JavaPlugin {
+
+    @Getter
+    public static StrawCosmetics INSTANCE;
+
+    @Override
+    public void onEnable() {
+        long begin = System.currentTimeMillis();
+        INSTANCE = this;
+        log.info("######################## [StrawCosmetics - " + getDescription().getVersion() + "] #################################");
+
+        log.info("Plugin enabled in " + (System.currentTimeMillis() - begin) + " ms.");
+        log.info("######################## [StrawCosmetics - " + getDescription().getVersion() + "] #################################");
+    }
 }
