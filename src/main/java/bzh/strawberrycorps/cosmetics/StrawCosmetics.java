b784ed77bd5156c25aed760ab4d60aa2b116be70
Uicias <clementbrd22@gmail.com>
2022-01-09 22:01:03 +0100

    👔 created titles basis

--- src/main/java/bzh/strawberrycorps/cosmetics/StrawCosmetics.java
+++ src/main/java/bzh/strawberrycorps/cosmetics/StrawCosmetics.java
@@ -1,8 +1,11 @@
 package bzh.strawberrycorps.cosmetics;
 
+import bzh.strawberrycorps.cosmetics.titles.TitleManager;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 @Log4j
 public class StrawCosmetics extends JavaPlugin {
@@ -10,11 +13,19 @@ public class StrawCosmetics extends JavaPlugin {
     @Getter
     public static StrawCosmetics INSTANCE;
 
+    @Getter
+    public static TitleManager titleManager;
+
     @Override
     public void onEnable() {
         long begin = System.currentTimeMillis();
         INSTANCE = this;
         log.info("######################## [StrawCosmetics - " + getDescription().getVersion() + "] #################################");
+        log.info("######################## [Plugin by - " + Arrays.toString(this.getDescription().getAuthors().toArray()) + "] #################################");
+
+        log.info("Started loading titles");
+        titleManager = TitleManager.getInstance();
+        log.info("Loaded titles");
 
         log.info("Plugin enabled in " + (System.currentTimeMillis() - begin) + " ms.");
         log.info("######################## [StrawCosmetics - " + getDescription().getVersion() + "] #################################");
---
+++ src/main/java/bzh/strawberrycorps/cosmetics/titles/TitleManager.java
@@ -0,0 +1,44 @@
+package bzh.strawberrycorps.cosmetics.titles;
+
+import bzh.strawberrycorps.cosmetics.StrawCosmetics;
+import bzh.strawberrycorps.cosmetics.titles.manager.Title;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/*
+This file titleManager is part of a project StrawCosmetics.
+It was created on 09/01/2022 at 21:19 by Uicias.
+This file as the whole project shouldn't be modified by others without the express permission from StrawCosmetics author(s).
+Also, this comment shouldn't get remove from the file. (see Licence.md)
+*/
+
+public class TitleManager {
+
+    private List<Title> titles;
+
+    private static TitleManager INSTANCE;
+
+    private TitleManager(){
+        this.titles = new ArrayList<>();
+        this.readConfig();
+    }
+
+    public static TitleManager getInstance(){
+        if(INSTANCE == null)
+            new TitleManager();
+        return INSTANCE;
+    }
+
+    private void readConfig(){
+        for(String flatName: Objects.requireNonNull(StrawCosmetics.getINSTANCE().getConfig().getConfigurationSection("titles")).getKeys(false)){
+            //todo
+        }
+    }
+
+    public Title getTitleByFlatName(String flatName){
+        return this.titles.stream().filter(title -> title.getFlatName().equalsIgnoreCase(flatName)).findFirst().orElse(null);
+    }
+
+}
