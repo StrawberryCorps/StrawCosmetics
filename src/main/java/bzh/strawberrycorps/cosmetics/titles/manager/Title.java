e17f2c57ced4f1ecff71f7cff63571e7e1e0e8ab
Eclixal <28450187+eclixal@users.noreply.github.com>
2022-01-10 08:23:42 +0100

    KABOUM

--- src/main/java/bzh/strawberrycorps/cosmetics/titles/manager/Title.java
+++ src/main/java/bzh/strawberrycorps/cosmetics/titles/manager/Title.java
@@ -1,56 +1,21 @@
 package bzh.strawberrycorps.cosmetics.titles.manager;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
 /*
 This file Title is part of a project StrawCosmetics.
 It was created on 09/01/2022 at 21:20 by Uicias.
 This file as the whole project shouldn't be modified by others without the express permission from StrawCosmetics author(s).
 Also, this comment shouldn't get remove from the file. (see Licence.md)
 */
-
+@AllArgsConstructor
+@Data
+@ToString
 public class Title {
 
     private int id;
-
     private String flatName;
-
     private String displayName;
-
-    public Title(int id, String flatName, String displayName) {
-        this.id = id;
-        this.flatName = flatName;
-        this.displayName = displayName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFlatName() {
-        return flatName;
-    }
-
-    public void setFlatName(String flatName) {
-        this.flatName = flatName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @Override
-    public String toString() {
-        return "Title{" +
-                "id=" + id +
-                ", flatName='" + flatName + '\'' +
-                ", displayName='" + displayName + '\'' +
-                '}';
-    }
 }
