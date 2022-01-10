9ba61697a2993c73375b1eaf987f7047379807f6
Eclixal <28450187+eclixal@users.noreply.github.com>
2022-01-10 18:30:52 +0100

    code format

--- src/main/java/bzh/strawberrycorps/cosmetics/titles/TitleManager.java
+++ src/main/java/bzh/strawberrycorps/cosmetics/titles/TitleManager.java
@@ -13,31 +13,30 @@ It was created on 09/01/2022 at 21:19 by Uicias.
 This file as the whole project shouldn't be modified by others without the express permission from StrawCosmetics author(s).
 Also, this comment shouldn't get remove from the file. (see Licence.md)
 */
-
 public class TitleManager {
 
     private List<Title> titles;
 
     private static TitleManager INSTANCE;
 
-    private TitleManager(){
+    private TitleManager() {
         this.titles = new ArrayList<>();
         this.readConfig();
     }
 
-    public static TitleManager getInstance(){
-        if(INSTANCE == null)
+    public static TitleManager getInstance() {
+        if (INSTANCE == null)
             new TitleManager();
         return INSTANCE;
     }
 
-    private void readConfig(){
-        for(String flatName: Objects.requireNonNull(StrawCosmetics.getINSTANCE().getConfig().getConfigurationSection("titles")).getKeys(false)){
+    private void readConfig() {
+        for (String flatName : Objects.requireNonNull(StrawCosmetics.getINSTANCE().getConfig().getConfigurationSection("titles")).getKeys(false)) {
             //todo
         }
     }
 
-    public Title getTitleByFlatName(String flatName){
+    public Title getTitleByFlatName(String flatName) {
         return this.titles.stream().filter(title -> title.getFlatName().equalsIgnoreCase(flatName)).findFirst().orElse(null);
     }
 
