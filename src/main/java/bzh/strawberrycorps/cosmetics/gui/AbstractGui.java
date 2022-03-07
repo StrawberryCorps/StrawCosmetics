0fea3d373c84e4f5aa792ecb80eb5e592665ae1e
Eclixal <28450187+eclixal@users.noreply.github.com>
2022-03-07 23:12:25 +0100

    work

---
+++ src/main/java/bzh/strawberrycorps/cosmetics/gui/AbstractGui.java
@@ -0,0 +1,11 @@
+package bzh.strawberrycorps.cosmetics.gui;
+
+/**
+ * This file AbstractGui is part of a project StrawTitles.
+ * It was created on O7/03/2022 18:41 by Eclixal.
+ * This file as the whole project shouldn't be modified by others without the express permission from StrawberryCorps author(s).
+ * Also, this comment shouldn't get remove from the file. (see Licence)
+ */
+
+public abstract class AbstractGui {
+}
